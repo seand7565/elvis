04f563aba8a6f1268bce60ce54483657c90808dd
Sean <seand7565@gmail.com>
2021-03-15 08:32:49 -0500

    Fix weird class name

--- lib/elvis/messages/weird.rb
+++ lib/elvis/messages/weird.rb
@@ -1,6 +1,6 @@
 module Elvis
   module Messages
-    class Regions
+    class Weird
       def self.text(user)
         "Let's get weird. What's a song you've been listening to recently that's a bit odd?"
       end
